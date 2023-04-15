a0851195e404926c4b17637651f47a6dc21ef3dc
Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>
2023-04-15 21:31:52 +0530

    user profile
    Signed-off-by: Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>

---
+++ src/app/components/user/user.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule
+  ]
+})
+export class UserModule { }
