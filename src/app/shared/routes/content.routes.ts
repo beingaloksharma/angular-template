5fec1cd119411e0f3e9d54780bbe385b945c4943
Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>
2023-04-15 12:24:41 +0530

    Userprofile
    Signed-off-by: Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>

--- src/app/shared/routes/content.routes.ts
+++ src/app/shared/routes/content.routes.ts
@@ -2,5 +2,4 @@ import { Routes } from "@angular/router";
 
 export const contentRoutes: Routes = [
     { path: 'books', loadChildren: () => import('../../components/book/book.module').then(m => m.BookModule) },
-    { path: 'user', loadChildren: () => import('../../components/userprofile/userprofile.module').then(m => m.UserprofileModule) },
 ];
