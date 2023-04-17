50b30bdc43b5bf05b3e72da88b498bfe96db28a0
Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>
2023-04-17 07:13:35 +0530

    removed user module
    Signed-off-by: Alok Kumar <18142121+beingaloksharma@users.noreply.github.com>

--- src/app/shared/routes/content.routes.ts
+++ src/app/shared/routes/content.routes.ts
@@ -2,5 +2,4 @@ import { Routes } from "@angular/router";
 
 export const contentRoutes: Routes = [
     { path: 'books', loadChildren: () => import('../../components/book/book.module').then(m => m.BookModule) },
-    { path: 'user', loadChildren: () => import('../../components/user/user.module').then(m => m.UserModule) },
 ];
